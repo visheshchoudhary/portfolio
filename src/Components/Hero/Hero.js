ed9ed07dbbac076dc02a07b2145f6ac006337034
visheshchoudhary <115351437+visheshchoudhary@users.noreply.github.com>
2025-11-07 01:37:03 +0530

    Update Hero component with new content and animations

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -32,8 +32,8 @@ const Hero = () => {
       <div id="hero1" className="hero__content">
         {/* Hero Name with AOS Animation */}
         <div {...getAosData("fade-up", 0)} className="hero_name">
-          <h1 className="hero_name1">Vishesh Choudhary</h1>
-          <h3 className="hero_name2">B.Tech CSE</h3>
+          <h1 className="hero_name1">Aryan Choudhary</h1>
+          <h3 className="hero_name2">MCA</h3>
         </div>
 
         {/* Education Section with AOS Animation */}
@@ -44,7 +44,7 @@ const Hero = () => {
         {/* About Section with AOS Animation */}
         <div {...getAosData("fade-up", 200)} className="hero_about">
           <h1 className="hero_about1">
-            I am a dedicated and enthusiastic professional with a strong passion for technology and a hunger for continuous learning. Eager to explore diverse job roles within computer science and engineering, I actively update my skills and stay abreast of industry advancements. Committed to personal and professional growth in a dynamic and evolving field, I consistently apply the concepts learned in my classes to create projects, which I update on Github and Linkedin. Additionally, I am a consistent coder on Leetcode and achieved the April Batch of 2024, and I am still actively participating.
+            I am a dedicated and enthusiastic professional with a strong passion for technology and a hunger for continuous learning. Eager to explore diverse job roles within computer science and engineering, I actively update my skills and stay abreast of industry advancements. Committed to personal and professional growth in a dynamic and evolving field, I consistently apply the concepts learned in my classes to create projects, which I update on Github and Linkedin. 
           </h1>
         </div>
 
@@ -52,7 +52,7 @@ const Hero = () => {
         <div {...getAosData("fade-up", 300)} className="hero_links">
           <div className="hero_link_btn">
             <img src={resume} className="hero_link_btn_img" alt="Resume" />
-            <a className="hero_link_btn_text" href="https://drive.google.com/file/d/1rBU1VFSnRQSC1F-BeTQwQGDZhjNddZeB/vie">Resume</a>
+            <a className="hero_link_btn_text" href="https://drive.google.com/file/d/1rBU1VFSnRQSC1F-BeTQwQGDZhjNddZe/vie">Resume</a>
           </div>
           <div className="hero_link_btn1">
             <a href="https://www.linkedin.com/in/aryan-choudhary-68205a383"><img  src={linkedin} className="hero_link_btn_img1" alt="LinkedIn" /></a>
@@ -66,4 +66,5 @@ const Hero = () => {
 }
 
 export default Hero;
+
 
